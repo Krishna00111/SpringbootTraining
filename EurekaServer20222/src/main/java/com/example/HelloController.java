824139e46396d16894a59440c407dbdc819a0129
Chinmay Deshmukh <42055861+Krishna00111@users.noreply.github.com>
2023-02-21 17:55:09 +0530

    Add files via upload

---
+++ EurekaServer20222/src/main/java/com/example/HelloController.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+//
+//
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String sayHello() {
+		return "Hello, this is from Hello Controller";
+	}
+	@RequestMapping("/")
+	public String welcome() {
+	return "Here page.....changing this.....";
+  }
+	@RequestMapping("/")
+	public String welcome2() {
+		return "Hello";
+	}
+}
+
