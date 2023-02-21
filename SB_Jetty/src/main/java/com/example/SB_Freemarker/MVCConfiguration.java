02fd19fb6f46d2beb4daf68a99510f7c698188f5
Chinmay Deshmukh <42055861+Krishna00111@users.noreply.github.com>
2023-02-21 18:06:49 +0530

    Add files via upload

---
+++ SB_Jetty/src/main/java/com/example/SB_Freemarker/MVCConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.SB_Freemarker;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class MVCConfiguration exterds WebMvConfigureAdapter
+{
+	@Override
+	public void configureViewResolver(ViewResolverRegistry registry) {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/jsp/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(jstlView.class);
+		registry.viewResolver(resolver);
+	}
+			
+
+}
---
+++ SB_Jetty/src/main/java/com/example/SB_Freemarker/SpringJSPApplication.java
@@ -0,0 +1,14 @@
+package com.example.SB_Freemarker;
+
+import org.springframework.boot.SpringApplication;
+
+public class SpringJSPApplication {
+	
+	
+	@SpringBootApplication
+	public class SpringJSApplication {
+		public static void main(String[] args)
+		    SpringApplication.run(SpringJSApplication.class, args)
+	}
+
+}
